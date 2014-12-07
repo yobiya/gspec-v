2086b43211d58be8e55d03eb195523b1f05e32ba
YoBiya <dummy@mail.com>
2014-12-07 20:48:27 +0900

    コミット処理のコードを整理

--- public/javascripts/commit.js
+++ public/javascripts/commit.js
@@ -3,10 +3,61 @@
  */
 var gspecv = gspecv || {};
 
-/**
- * @brief コミット処理のセットアップ
- */
-gspecv.setupCommit = function(selecters) {
+gspecv.commit = (function() {
+  var selecters;
+  var commitFiles = createFileArray();
+
+  /**
+   * @brief コミット処理のセットアップ
+   */
+  function setup(selecterObjects) {
+    selecters = selecterObjects;
+
+    $fileNames = selecters.$commitBox.find('#file_names');
+    $cancelButton = selecters.$commitBox.find('#cancel_button');
+    $execButton = selecters.$commitBox.find('#commit_button');
+    $commentTextArea = selecters.$commitBox.find('#comment_text');
+
+    // ページ全体にファイルをドロップされてもブラウザが処理を行わないように無視する
+    $('html').on('drop', function(e) {
+      e.preventDefault();
+    })
+    .on('dragover', function(e) {
+      e.preventDefault();
+    });
+
+    // コミット用のドロップ領域にファイルがドロップされた場合の処理
+    selecters.$commitBox.on('dragover', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    })
+    .on('drop', function(e) {
+      var files = e.originalEvent.dataTransfer.files;
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      // ファイルを配列に追加
+      for(var i = 0; i < files.length; i++) {
+        commitFiles.add(files[i]);
+      }
+
+      // 表示を更新
+      $fileNames.empty();
+      commitFiles.forEach(function(file) {
+        $fileNames.append('<p>' + file.name + '</p>');
+      });
+      $cancelButton.removeAttr('disabled');
+      $execButton.removeAttr('disabled');
+    });
+
+    $cancelButton.on('click', clearCommit); // コミットのキャンセル
+    $execButton.on('click', doCommit);      // コミットの実行
+
+    // 初期化としてクリア処理を行う
+    clearCommit();
+  }
+
   function createFileArray() {
     var array = [];
     array.add = function(file) {
@@ -27,55 +78,13 @@ gspecv.setupCommit = function(selecters) {
     return array;
   }
 
-  var commitFiles = createFileArray();
-  $fileNames = selecters.$commitBox.find('#file_names');
-  $cancelButton = selecters.$commitBox.find('#cancel_button');
-  $execButton = selecters.$commitBox.find('#commit_button');
-  $commentTextArea = selecters.$commitBox.find('#comment_text');
-
-  // ページ全体にファイルをドロップされてもブラウザが処理を行わないように無視する
-  $('html').on('drop', function(e) {
-    e.preventDefault();
-  })
-  .on('dragover', function(e) {
-    e.preventDefault();
-  });
-
-  // コミット用のドロップ領域にファイルがドロップされた場合の処理
-  selecters.$commitBox.on('dragover', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-  })
-  .on('drop', function(e) {
-    var files = e.originalEvent.dataTransfer.files;
-
-    e.preventDefault();
-    e.stopPropagation();
-
-    // ファイルを配列に追加
-    for(var i = 0; i < files.length; i++) {
-      commitFiles.add(files[i]);
-    }
-
-    // 表示を更新
-    $fileNames.empty();
-    commitFiles.forEach(function(file) {
-      $fileNames.append('<p>' + file.name + '</p>');
-    });
-    $cancelButton.removeAttr('disabled');
-    $execButton.removeAttr('disabled');
-  });
-
-  $cancelButton.on('click', clearCommit); // コミットのキャンセル
-  $execButton.on('click', doCommit);      // コミットの実行
-
   /**
    * @brief コミット情報をクリアする
    */
   function clearCommit() {
     commitFiles = createFileArray();
     $fileNames.empty();
-    $fileNames.append($('<p></p>').text('Drop file here').addClass('hint_text'));
+    $fileNames.append($('<p>').text('ここにファイルをドロップ').addClass('hint_text'));
     $commentTextArea.val('');
   }
 
@@ -111,8 +120,8 @@ gspecv.setupCommit = function(selecters) {
     });
   }
 
-  // 初期化としてクリア処理を行う
-  clearCommit();
-
-};
+  return {
+    setup: setup
+  };
+})();
 
