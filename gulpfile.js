c4f4b7461f7c96bd0c9e4deae3f8be374aaf06a5
YoshizawaTakeshi <tyoshizawa@YoshizawaTakeshi-no-MacBook-Pro.local>
2015-02-23 00:01:16 +0900

    gulpでtypescriptをビルドするように設定

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
+var typescript = require('gulp-typescript');
 
-gulp.task('hello', function() {
-  console.log('Hello gulp!');
+gulp.task('browser-typescript', function() {
+  gulp
+    .src('src/browser/ts/*.ts')
+    .pipe(typescript({ target: 'ES5', module: 'commonjs' }))
+    .js
+    .pipe(gulp.dest('public/javascripts'));
 });
 
-gulp.task('default', ['hello']);
+gulp.task('default', ['browser-typescript']);
