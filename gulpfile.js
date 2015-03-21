4991369275c56be31c8f8dca0cb338f3040402bb
YoshizawaTakeshi <tyoshizawa@YoshizawaTakeshi-no-MacBook-Pro.local>
2015-03-21 22:59:50 +0900

    gulpにwebpackのビルド処理を追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require('gulp');
 var typescript = require('gulp-typescript');
+var webpack = require('gulp-webpack');
 
 gulp.task('browser-typescript', function() {
   gulp
     .src('src/common/*.ts', { base: 'src/common' })
-    .pipe(gulp.dest('src/browser/_common'));
+    .pipe(gulp.dest('src/browser/ts/_common'));
 
   gulp
     .src('src/browser/ts/**/*.ts')
     .pipe(typescript({ target: 'ES5', module: 'commonjs' }))
     .js
     .pipe(gulp.dest('temp/browser'));
+
+  gulp
+    .src('./temp/browser/main_page.js')
+    .pipe(
+      webpack({
+        output: {
+          filename: 'packed.js',
+        },
+      }))
+    .pipe(gulp.dest('./public/javascripts'));
 });
 
 gulp.task('server-typescript', function() {
